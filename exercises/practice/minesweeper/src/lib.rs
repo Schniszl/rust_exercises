49841ad226a11902f090ba47d1ea225d12b5bcaa
David <daviddao1997@gmail.com>
2025-04-19 16:40:16 +0200

    minesweeper solution version 3

--- exercises/practice/minesweeper/src/lib.rs
+++ exercises/practice/minesweeper/src/lib.rs
@@ -1,28 +1,34 @@
 extern crate alloc;
 
-use alloc::string::{String};
+use alloc::string::{String, ToString};
 use alloc::vec;
 use alloc::vec::Vec;
+use core::convert::From;
 use core::iter::Iterator;
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-
+    let mut field : Vec<String> = Vec::new();
     if minefield.is_empty() {
-        return vec![]
+        return field
     }
 
     let rows = minefield.len();
     let cols = if rows > 0 { minefield[0].len() } else { 0 };
 
-    // Erstelle eine liste über liste mit chars
-    // let mut grid: Vec<Vec<char>> = minefield.iter().map(|s| s.chars().collect()).collect();
-    let mut grid: Vec<Vec<char>> = Vec::new();
+    field = vec![String::from(" ".repeat(minefield[0].len()));minefield.len()];
 
-    for line in minefield {
-        let chars: Vec<char> = line.chars().collect(); // zerlege Zeile in Zeichen
-        grid.push(chars); // füge die Zeichenzeile der Liste hinzu
+    for (row_index, row) in minefield.iter().enumerate() {
+        for(col_index, chr) in row.chars().enumerate(){
+            if chr == '*' {
+                field =mine_found(field, row_index, col_index,rows,cols);
+
+            }
+        }
     }
+    field
+}
 
+fn mine_found(mut field: Vec<String>, row_index: usize,col_index: usize, rows: usize, cols: usize ) -> Vec<String> {
     // Leichter zugriff auf die Nachbarknoten
     let directions = [
         (-1, -1), (-1, 0), (-1, 1),
@@ -30,49 +36,27 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
         ( 1, -1), ( 1, 0), ( 1, 1),
     ];
 
-    for i in 0..rows {
-        for j in 0..cols {
-            // Wenn Mine gefunden wurde
-            if grid[i][j] == '*' {
+    field[row_index].replace_range(col_index..col_index+1,"*");
+    for (iter_row, iter_col) in directions {
+        let ni = row_index as isize + iter_row;
+        let nj = col_index as isize + iter_col;
 
-                for (iter_row, iter_col) in directions {
-                    // errechne den nachbar wert ->isize, da auch negative werte vorkommen
-                    let ni = i as isize + iter_row;
-                    let nj = j as isize + iter_col;
-                    // prüfe ob es ein nachbarn gibt
-                    if ni >= 0 && ni < rows as isize && nj >= 0 && nj < cols as isize {
-                        // Konvertiere ni und nj in unsigned, da vektoren positiv sein müssen
-                        let (ni, nj) = (ni as usize, nj as usize);
+        if ni >= 0 && ni < rows as isize && nj >= 0 && nj < cols as isize {
+            //// Konvertiere ni und nj in unsigned, da vektoren positiv sein müssen
+            let (ni,nj) = (ni as usize, nj as usize);
+            let c = field[ni].chars().nth(nj).unwrap();
 
-                        // Welche Nachbarn habe ich
-                        if grid[ni][nj] != '*' {
-                            let c = grid[ni][nj];
-                            if c.is_ascii_digit() {
-                                let n = c.to_digit(10).unwrap() + 1;
-                                grid[ni][nj] = std::char::from_digit(n, 10).unwrap();
-                            } else {
-                                grid[ni][nj] = '1';
-                            }
-                        }
-                    }
+            if c != '*' {
+                if c.is_digit(10){
+                    let n = c.to_digit(10).unwrap() + 1;
+                    field[ni].replace_range(nj..nj+1,n.to_string().as_str());
+
+                } else {
+                    field[ni].replace_range(nj..nj+1,"1");
                 }
             }
         }
     }
-    //Konvertiere die Liste über Liste wieder in eine Listvon Strings
-    let mut result = Vec::new();
-
-    for row in grid {
-        let mut row_string = String::new();
-        for ch in row {
-            row_string.push(ch); // füge jedes Zeichen zum String hinzu
-        }
-        result.push(row_string); // füge die fertige Zeile zum Ergebnis hinzu
-    }
-    result
-
-    // Mach aus der Liste über Liste mit Zeichen eine Liste von Strings
-    // grid.into_iter().map(|row| row.into_iter().collect()).collect()
+    field
 }
-
 
